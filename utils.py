a846855a53a33c05bdcf09158bff9e18b51a44c8
Rhys Gould <46973360+r-gould@users.noreply.github.com>
2022-08-25 19:20:13 +0100

    changed file path

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def save_samples(model, count, shape, idx=0):
     x_0 = model.sample(count, shape)
     nrow = math.ceil(math.sqrt(count))
     grid = make_grid(x_0, nrow=nrow)
-    save_image(grid, f"diffusion/images/generated/grid_{idx}.png")
+    save_image(grid, f"images/generated/grid_{idx}.png")
 
 def plot_stats(train_losses, valid_losses):
 
@@ -16,14 +16,14 @@ def plot_stats(train_losses, valid_losses):
     plt.ylabel("Loss")
     plt.xlabel("Epochs")
     plt.plot(train_losses)
-    plt.savefig("diffusion/images/plots/train.png")
+    plt.savefig("images/plots/train.png")
     plt.show()
 
     plt.title("Valid losses")
     plt.ylabel("Loss")
     plt.xlabel("Epochs")
     plt.plot(valid_losses)
-    plt.savefig("diffusion/images/plots/valid.png")
+    plt.savefig("images/plots/valid.png")
     plt.show()
 
     plt.title("Losses")
@@ -32,5 +32,5 @@ def plot_stats(train_losses, valid_losses):
     plt.plot(train_losses, label="Train")
     plt.plot(valid_losses, label="Valid")
     plt.legend(loc="upper right")
-    plt.savefig("diffusion/images/plots/both.png")
+    plt.savefig("images/plots/both.png")
     plt.show()
