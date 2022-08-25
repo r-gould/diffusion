5f0c7f8357413b9b97a8c5d2867fda74829b5eee
Rhys Gould <46973360+r-gould@users.noreply.github.com>
2022-08-25 19:21:05 +0100

    changed file path

--- load.py
+++ load.py
@@ -9,8 +9,8 @@ def load(torch_dataset, input_channels, batch_size):
                              std=(0.5,) * input_channels),
     ])
 
-    train_ds = torch_dataset(root="diffusion/data/", train=True, download=True, transform=transform)
-    valid_ds = torch_dataset(root="diffusion/data/", train=False, download=True, transform=transform)
+    train_ds = torch_dataset(root="data/", train=True, download=True, transform=transform)
+    valid_ds = torch_dataset(root="data/", train=False, download=True, transform=transform)
     
     train_dl = DataLoader(train_ds, batch_size, shuffle=True)
     valid_dl = DataLoader(valid_ds, batch_size, shuffle=True)
